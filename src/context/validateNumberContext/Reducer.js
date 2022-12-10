8b717463bc9fcea37d56661426533650c9ca0985
jesuanp <jespaterm@gmail.com>
2022-12-10 09:38:47 -0500

    trayendo el codigo de verificacion del numero

--- src/context/validateNumberContext/Reducer.js
+++ src/context/validateNumberContext/Reducer.js
@@ -1,5 +1,12 @@
+import { CHANGE_CODE } from '../../types';
+
 export default (state, action) => {
     switch(action.type){
+
+        case CHANGE_CODE: return {
+            ...state,
+            code: action.payload
+        }
 
         default: return state;
     }
--- src/context/validateNumberContext/State.js
+++ src/context/validateNumberContext/State.js
@@ -1,20 +1,43 @@
 import { useReducer } from 'react';
 import ValidateNumberContext from './Context';
 import ValidateNumberReducer from './Reducer';
+import clientAxios from '../../config/axios';
+import { CHANGE_CODE } from '../../types';
 
 
 const ValidateNumberState = ({children}) => {
 
     const initialState = {
-        code: '123456'
+        code: ''
     }
 
     const [state, dispatch] = useReducer(ValidateNumberReducer, initialState);
 
+    const sendMessage = async (phoneNumber) => {
+
+        try{
+            
+            const response = await clientAxios.post('/validateNumber', {phoneNumber});
+
+            console.log(response.data.code);
+
+            dispatch({
+                type: CHANGE_CODE,
+                payload: response.data.code
+            })
+        }
+        catch(e){
+            console.log(e);
+        }
+
+    }
+
     return (
         <ValidateNumberContext.Provider
             value={{
-                code: state.code
+                code: state.code,
+
+                sendMessage
             }}
         >
             {children}

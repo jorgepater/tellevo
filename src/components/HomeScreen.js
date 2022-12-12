a5fd87f7eacf245ba11d0ba3189fc6dfbd791327
jesuanp <jespaterm@gmail.com>
2022-12-12 00:38:30 -0500

    mostrando los datos de usurio para probrar las rutas

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,15 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Link } from '@react-navigation/native';
+import AuthContext from '../context/authContext/AuthContext';
+import { useContext } from 'react';
 
 export default function HomeScreen() {
+
+  const {user} = useContext(AuthContext);
+
+  console.log(user);
+
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
       <StatusBar style="auto" />
+      <Text>Home</Text>
       <Link to={{ screen: 'Profile' }}>
         Go to Profile
       </Link>
+
+      <Text style={{marginTop: 5}}>
+        nombre: {user.firstName}
+      </Text>
+      <Text>
+        apellido: {user.lastName}
+      </Text>
+      <Text>
+        telefono: {user.phoneNumber}
+      </Text>
+      <Text>
+        {user.document.documentType}: {user.document.documentNumber}
+      </Text>
+      <Text>
+        fecha de expedicion: {user.document.dateExp}
+      </Text>
     </View>
   );
 }
